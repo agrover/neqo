93615f1b6660e9c5ed082ca68e9344b031f7a441
Andy Grover <agrover@mozilla.com>
2020-01-17 14:02:10 -0800

    Support packet sent
    Pull out frame and pkt type conversion into helper functions.

--- neqo-transport/src/connection.rs
+++ neqo-transport/src/connection.rs
@@ -1270,6 +1270,7 @@ impl Connection {
                 SentPacket::new(now, ack_eliciting, tokens, packet.len(), in_flight),
             );
 
+            qlog::packet_sent(&self.qlog, now, &hdr, &encoder);
             dump_packet(self, "TX ->", &hdr, &encoder);
 
             out_bytes.append(&mut packet);
